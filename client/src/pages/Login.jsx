f116fe09e9a9b4c9ca7fe2ec1c74e543304ac2a5
Divyanka Rai <raidivyanka40@gmail.com>
2023-01-24 19:33:35 +0530

    login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,30 +1,99 @@
-import React,{useState} from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink , useNavigate } from "react-router-dom";
 
 const Login = () => {
 
-  const [passShow, setpassShow] = useState(false)
+  const nav = useNavigate()
+  const [passShow, setpassShow] = useState(false);
+
+   const [inpVal, setinpVal] = useState({
+      email: "",
+      password: ""
+   })
+
+  const setVal = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setinpVal({ ...inpVal, [name]: value });
+  };
+
+  console.log(inpVal);
+
+  const addUserData = () =>{
+   
+  }
 
   return (
-    <div style={{width:"300px",height:"280px", border: "2px solid black", margin:"auto",textAlign:"center",marginTop: "5%",backgroundColor:"pink"
-    }}>
+    <div
+      style={{
+        width: "300px",
+        height: "280px",
+        border: "2px solid black",
+        margin: "auto",
+        textAlign: "center",
+        marginTop: "5%",
+        backgroundColor: "pink",
+      }}
+    >
       <form>
         <h3>Enter your Email</h3>
-        <div  style={{backgroundColor:"white", height:"30px",width:"80%",margin:"auto"}}>
-        <input style={{border:"none", textAlign:"center"}} type="email" name="email" placeholder='enter your email' />
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{
+              border: "none",
+              height: "100%",
+              width: "100%",
+              textAlign: "center",
+            }}
+            type="email"
+            name="email"
+            placeholder="enter your email"
+             value={inpVal.email}
+            onChange={setVal}
+          />
         </div>
-          <h3>Enter your password</h3>
-        <div style={{display:"flex",backgroundColor:"white",height:"30px",width:"80%",margin:"auto"}}>
-          <input style={{border:"none",marginLeft: "3%"}} type={!passShow ? "password" : "text"} name="password" placeholder='enter your password' />
-          <div style={{marginLeft:"5%",marginTop:"2%",cursor:"pointer"}} onClick={()=>{setpassShow(!passShow)}}>
+        <h3>Enter your password</h3>
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{ border: "none", marginLeft: "3%" }}
+            type={!passShow ? "password" : "text"}
+            name="password"
+            placeholder="enter your password"
+             value={inpVal.password}
+            onChange={setVal}
+          />
+          <div
+            style={{ marginLeft: "5%", marginTop: "2%", cursor: "pointer" }}
+            onClick={() => {
+              setpassShow(!passShow);
+            }}
+          >
             {!passShow ? "Show" : "Hide"}
           </div>
-        </div><br/>
-        <button>Login</button>
-        <p>Don't have an account? <NavLink to="/register">Sign-Up</NavLink></p>
+        </div>
+        <br />
+        <button onClick={addUserData}>Login</button>
+        <p>
+          Don't have an account? <NavLink to="/register">Sign-Up</NavLink>
+        </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,56 +1,157 @@
-import React,{useState} from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [passShow, setpassShow] = useState(false)
-  const [cpassShow, setcpassShow] = useState(false)
+
+  const nav = useNavigate()
+  const [passShow, setpassShow] = useState(false);
+  const [cpassShow, setcpassShow] = useState(false);
 
   const [inpVal, setinpVal] = useState({
-    name:"",
-    email:"",
-    password:"",
-    cpassword:""
-  })
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
 
-  const setVal=(e)=>{
-    const {name,value} = e.target 
-    console.log(name,value);
-    setinpVal({...inpVal,[name]:value})
-  }
+  const setVal = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setinpVal({ ...inpVal, [name]: value });
+  };
 
   console.log(inpVal);
 
+  const addUserData = () =>{
+    const {name,email,password,cpassword} = inpVal 
+    if(password != cpassword){
+      alert("password doesn't match")
+    }
+    else(alert("registered successfully"))
+    nav("/")
+  }
+
   return (
-    <div style={{width:"300px",height:"auto", border: "2px solid black", margin:"auto",textAlign:"center",marginTop: "5%",backgroundColor:"pink"
-    }}>
+    <div
+      style={{
+        width: "300px",
+        height: "auto",
+        border: "2px solid black",
+        margin: "auto",
+        textAlign: "center",
+        marginTop: "5%",
+        backgroundColor: "pink",
+      }}
+    >
       <form>
-      <h3>Enter yourName</h3>
-        <div style={{backgroundColor:"white", height:"30px",width:"80%",margin:"auto"}}>
-        <input style={{border:"none",height:"100%",width:"100%", textAlign:"center"}} type="text" name="name" placeholder='enter your name' onChange={setVal} />
+        <h3>Enter yourName</h3>
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{
+              border: "none",
+              height: "100%",
+              width: "100%",
+              textAlign: "center",
+            }}
+            type="text"
+            name="name"
+            value={inpVal.name}
+            placeholder="enter your name"
+            onChange={setVal}
+          />
         </div>
         <h3>Enter your Email</h3>
-        <div style={{backgroundColor:"white", height:"30px",width:"80%",margin:"auto"}}>
-        <input style={{border:"none", height:"100%",width:"100%", textAlign:"center"}} type="email" name="email" placeholder='enter your email' onChange={setVal} />
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{
+              border: "none",
+              height: "100%",
+              width: "100%",
+              textAlign: "center",
+            }}
+            type="email"
+            name="email"
+            value={inpVal.email}
+            placeholder="enter your email"
+            onChange={setVal}
+          />
         </div>
-          <h3>Enter your password</h3>
-        <div style={{display:"flex",backgroundColor:"white",height:"30px",width:"80%",margin:"auto"}}>
-          <input style={{border:"none",marginLeft: "3%"}} type={!passShow ? "password" : "text"} name="password" placeholder='enter your password' onChange={setVal} />
-          <div style={{marginLeft:"5%",marginTop:"2%",cursor:"pointer"}} onClick={()=>{setpassShow(!passShow)}}>
+        <h3>Enter your password</h3>
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{ border: "none", marginLeft: "3%" }}
+            type={!passShow ? "password" : "text"}
+            name="password"
+            placeholder="enter your password"
+            value={inpVal.password}
+            onChange={setVal}
+          />
+          <div
+            style={{ marginLeft: "5%", marginTop: "2%", cursor: "pointer" }}
+            onClick={() => {
+              setpassShow(!passShow);
+            }}
+          >
             {!passShow ? "Show" : "Hide"}
           </div>
         </div>
         <h3>Enter Confirm password</h3>
-        <div style={{display:"flex",backgroundColor:"white",height:"30px",width:"80%",margin:"auto"}}>
-          <input style={{border:"none",marginLeft: "3%"}} type={!cpassShow ? "password" : "text"} name="cpassword" placeholder='enter your Cpassword' onChange={setVal} />
-          <div style={{marginLeft:"5%",marginTop:"2%",cursor:"pointer"}} onClick={()=>{setcpassShow(!cpassShow)}}>
+        <div
+          style={{
+            display: "flex",
+            backgroundColor: "white",
+            height: "30px",
+            width: "80%",
+            margin: "auto",
+          }}
+        >
+          <input
+            style={{ border: "none", marginLeft: "3%" }}
+            type={!cpassShow ? "password" : "text"}
+            name="cpassword"
+            placeholder="enter your Cpassword"
+            onChange={setVal}
+            value={inpVal.cpassword}
+          />
+          <div
+            style={{ marginLeft: "5%", marginTop: "2%", cursor: "pointer" }}
+            onClick={() => {
+              setcpassShow(!cpassShow);
+            }}
+          >
             {!cpassShow ? "Show" : "Hide"}
           </div>
-        </div><br/>
-        <button style={{marginBottom:"8%"}}>Login</button>
-        <p style={{marginTop:"-2%"}}>Already have an account? <NavLink to="/">Login</NavLink></p>
+        </div>
+        <br />
+        <button style={{ marginBottom: "8%" }} onClick={addUserData}>Login</button>
+        <p style={{ marginTop: "-2%" }}>
+          Already have an account? <NavLink to="/">Login</NavLink>
+        </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
